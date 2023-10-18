f7c7dd933490dfc5c9c270ad0bf9c4f2b036808f
Bohdan Panchenko <logitechchillstream@gmail.com>
2023-10-18 15:27:38 +0300

    visiters tracking

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
 const express = require("express");
-const fs = require("fs");
+const nodemailer = require("nodemailer");
+// const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
-const data = [];
+// const data = [];
+
+const emailConfig = {
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+};
+const transporter = nodemailer.createTransport(emailConfig);
+const recipientEmail = "logitechchillstream@gmail.com";
 
 app.get("/", (req, res) => {
-  fs.writeFile("hello.txt", "У вас новый посетитель!");
-  res.json("200");
-});
+  // const note = `У вас новый посетитель! ${getDate()} ip-address ${req.ip} \n`;
+  // data.push(note);
+  // data.forEach((el) => {
+  //   fs.appendFile("hello.txt", el, () => {});
+  // });
 
-app.post("/", (res, req) => {
-  fs.writeFile("hello.txt", "У вас новый посетитель!");
+  const mailOptions = {
+    from: emailConfig.auth.user,
+    to: recipientEmail,
+    subject: "Новый посетитель!",
+    text: `У вас новый посетитель! ${getDate()} ip-address ${req.ip} \n`,
+  };
+
+  transporter.sendMail(mailOptions);
+
+  res.send(200);
 });
 
 app.listen(port, () => {
   console.log("Server starting...");
 });
+
+function getDate() {
+  const date = new Date();
+  const hours = date.getDay();
+  const minutes =
+    date.getDay() < 10 ? `0${date.getDay()}` : `0${date.getDay()}`;
+  const seconds =
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : `0${date.getSeconds()}`;
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+// request.socket.remoteAddress
 module.exports = app;
 module.exports = fs;
