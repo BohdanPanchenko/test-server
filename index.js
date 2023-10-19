c47c15d13b8e6c19d0a72e1bfe4e77ebf21d54fe
Bohdan Panchenko <logitechchillstream@gmail.com>
2023-10-19 08:24:30 +0300

    checkVisiters function update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 const port = 3000;
 const jsonBodyMiddleware = express.json();
-app.set(jsonBodyMiddleware);
+app.use(jsonBodyMiddleware);
 
 const emailConfig = {
   service: "gmail",
