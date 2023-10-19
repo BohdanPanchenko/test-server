93e129c9c7504ce1411bc1f3a7064fd6a12276a6
Bohdan Panchenko <logitechchillstream@gmail.com>
2023-10-19 07:30:06 +0300

    method post added

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post("/", async (req, res) => {
       from: emailConfig.auth.user,
       to: recipientEmail,
       subject: "Новый посетитель!",
-      text: `У вас новый посетитель! ${getDate()} ip-address ${req.body.ip} \n`,
+      text: `У вас новый посетитель! ${getDate()} ip-address ${req.body} \n`,
     };
 
     await transporter.sendMail(mailOptions);
