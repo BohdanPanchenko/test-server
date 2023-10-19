d6e6d2030f13140a468419c6705f72ba1d1b5286
Bohdan Panchenko <logitechchillstream@gmail.com>
2023-10-19 08:13:19 +0300

    checkVisiters function update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post("/", async (req, res) => {
       from: emailConfig.auth.user,
       to: recipientEmail,
       subject: "Новый посетитель!",
-      text: `У вас новый посетитель! ${getDate()} ip-address ${req.body} \n`,
+      text: `У вас новый посетитель! ${getDate()} ip-address ${req.body.ip} \n`,
     };
 
     await transporter.sendMail(mailOptions);
